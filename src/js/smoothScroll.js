96549ae881b996ac59318db2193b57e48ed69d0e
Adijos <jedrczakadrian@gmail.com>
2019-02-14 15:19:43 +0100

    Fixed script smoothScroll

--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -2,8 +2,10 @@ function init() {
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
-      console.log(anchor);
 
+      const empty = anchor.getAttribute('href');
+
+      if (empty === '#') return;
       if (anchor === document.querySelector('li.navigation__item a[href="#info"]') || anchor === document.querySelector('li.navigation__item a[href="#contact"]') || anchor === document.querySelector('[href="#info"]') || anchor === document.querySelector('[href="#contact"]')) {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
           behavior: 'smooth',
